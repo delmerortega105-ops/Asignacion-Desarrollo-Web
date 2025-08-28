1f7592953699048c53c528c032205ea3859821d7
delmerortega105-ops <delmerortega105@gmail.com>
2025-08-27 20:14:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Instituto Ramón Rosa</title>
+  <link rel="stylesheet" href="css/estilo.css"/>
+  <link rel="stylesheet" href="fonts.css">
+</head>
+
+
+<body>
+
+
+
+  <header>
+
+<h1>INSTITUTO RAMON ROSA <br> 120600157M02</h1>
+  <h3>Santa Rosita, Guajiquiro, La Paz</h3>
+
+
+    <nav>
+      <ul>
+        <li><a href="#" target="blank"><span class="icon-quill"></span>Nosotros</a></li>
+        <li><a href="#" target="blank"><span class="icon-profile"></span>Carrera</a></li>
+        <li><a href="#" target="blank"><span class="icon-user-tie"></span>Contacto</a></li>
+      </ul>
+    </nav>
+
+
+  </header>
+
+
+
+  <main>
+
+
+    <section>
+      <h2>Bienvenido</h2>
+      <p>Instituto Ramón Rosa es la dependencia de la Secretaría de Educación, creada para atender a la 
+        población en el Nivel Medio, ofreciendo la oportunidad de cursar Bachilleratos que le permiten
+        incorporarse a la vida laboral del pais, por medio de una formación académica basada en criterios 
+        pedagógicos que garanticen el desarrollo de habilidades, tanto técnicas como humanísticas, buscando
+        una persona amante a su Patria y dispuesta por medio del trabajo productivo, contribuir en el desarrollo,
+        cultural, social, económico e integral de las comunidades.
+      </p>
+      <img src="img/LOGO.png" alt="">
+    </section>
+
+
+   
+      <div class="carousel-container">
+    <button class="prev" onclick="moveSlide(-1)">❮</button>
+    <div class="carousel">
+      <img src="img/foto1.jpg" alt="Foto 1">
+      <img src="img/foto2.jpg" alt="Foto 2">
+      <img src="img/foto3.jpg" alt="Foto 3">
+      <img src="img/foto4.jpg" alt="Foto 4">
+      <img src="img/foto5.jpg" alt="Foto 5">
+    </div>
+    <button class="next" onclick="moveSlide(1)">❯</button>
+  </div>
+
+  <script src="carrusel.js"></script>
+    
+
+
+  </main>
+
+  <footer>
+    <p>&copy; 2025 Mi Sitio Web Ana Rivera</p>
+  </footer>
+
+
+</body>
+</html>
